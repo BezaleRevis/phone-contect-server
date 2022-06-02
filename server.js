16cb46cbc52f3d588d597cc5092dcdf9d51aa854
BezaleRevis <bezalel803@gmail.com>
2022-06-02 04:52:40 +0300

    accses

--- server.js
+++ server.js
@@ -10,7 +10,12 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
 app.use(express.json());
 
 async function startServer() {
